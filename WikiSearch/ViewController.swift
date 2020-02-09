097738fcd8d38fef82ea425c62dc3a6318dece34
KemilaTharaka <kemiladesilva@gmail.com>
2020-02-09 12:33:04 +0530

    Initial Commit

---
+++ WikiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiSearch
+//
+//  Created by Kemila on 2/9/20.
+//  Copyright © 2020 Kemila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
